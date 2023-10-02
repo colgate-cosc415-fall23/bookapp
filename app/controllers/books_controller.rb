4c771592d0927c18d217788df38066475a50a8c5
jsommers <jsommers@colgate.edu>
2023-10-02 10:14:37 -0400

    new/create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,23 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(create_params)
+    if @book.save
+      redirect_to books_path and return
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def create_params
+    params.require(:book).permit(:title, :publisher, :year, :pages, :list_price)
+  end
+
 end
