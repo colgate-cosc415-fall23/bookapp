7b1f51dc649ede9b92ef468bf9ee82e1d83896cf
jsommers <jsommers@colgate.edu>
2023-10-13 10:22:02 -0400

    cucumber

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,26 @@
+Given('I am on the books page') do
+  visit books_path
+end
+  
+When('I fill in {string} with {string}') do |formentry, value|
+    fill_in formentry, with: value
+end
+  
+When('I press {string}') do |button|
+    click_on button
+end
+  
+Then('I should be on the books page') do
+    expect(page.current_path).to eq(books_path)
+end
+  
+Then('I should see {string}') do |string|
+    expect(page).to have_content(string)
+end
+
+Given(/^these Books:$/) do |table| # table is a Cucumber::Ast::Table  
+  debugger
+  table.hashes.each do |h|
+    Book.create!(h)
+  end
+end
