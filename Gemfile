92d9563102317fd8297b8531ab5c1c165e533a6d
jsommers <jsommers@colgate.edu>
2023-10-30 12:35:18 -0400

    add devise, db auth

--- Gemfile
+++ Gemfile
@@ -62,6 +62,8 @@ group :development, :test do
   gem 'rspec-rails'
 end
 
+gem 'devise'
+
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_191744) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_131449) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
@@ -52,6 +52,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_191744) do
     t.integer "review_author_id", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["review_author_id"], name: "index_reviews_on_review_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "reviews", "books"
